0eb7220db40ebaa82ec87062df0caf91b4494c01
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-04-29 19:20:11 +0530

    [MODIFIED]:shubham this UC-1 is Create Contact

--- src/com/shubham/addressbook/AddressBookMain.java
+++ src/com/shubham/addressbook/AddressBookMain.java
@@ -1,7 +1,11 @@
 package com.shubham.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
+
     public static void main(String[] args) {
         System.out.println("Welcome To Address Book");
+
     }
 }
