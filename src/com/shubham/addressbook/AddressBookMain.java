db489b9a82518a4f05164f9c826d1dae746e5aad
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-04-30 12:15:52 +0530

    [ADDED]:shubham This UC-2 for Add Contact to Address book

--- src/com/shubham/addressbook/AddressBookMain.java
+++ src/com/shubham/addressbook/AddressBookMain.java
@@ -4,8 +4,50 @@ import java.util.Scanner;
 
 public class AddressBookMain {
 
+    Scanner scanner = new Scanner(System.in);
+    Contact contact = new Contact();
+    public void inputContact() {
+        System.out.println("Enter the FirstName:");
+        contact.setFirstName(scanner.next());
+        System.out.println("Enter the LastName:");
+        contact.setLastName(scanner.next());
+        System.out.println("Enter the Address:");
+        contact.setAddress(scanner.next());
+        System.out.println("Enter the City:");
+        contact.setCity(scanner.next());
+        System.out.println("Enter the State:");
+        contact.setState(scanner.next());
+        System.out.println("Enter the Zipcode:");
+        contact.setZipcode(scanner.next());
+        System.out.println("Enter the PhoneNumber:");
+        contact.setPhoneNumber(scanner.next());
+        System.out.println("Enter the Email:");
+        contact.setEmail(scanner.next());
+    }
+
+    public void displayContact(){
+        System.out.println("FirstName:"+contact.getFirstName());
+        System.out.println("LastName:"+contact.getLastName());
+        System.out.println("Address:"+contact.getAddress());
+        System.out.println("City:"+contact.getCity());
+        System.out.println("State:"+contact.getState());
+        System.out.println("Zipcode:"+contact.getZipcode());
+        System.out.println("PhoneNumber:"+contact.getPhoneNumber());
+        System.out.println("Email:"+contact.getEmail());
+    }
+
+    public void display(){
+        System.out.println(contact.toString());
+
+    }
+
+
     public static void main(String[] args) {
         System.out.println("Welcome To Address Book");
+
+        AddressBookMain addressbookmain = new AddressBookMain();
+        addressbookmain.inputContact();
+        addressbookmain.displayContact();
 
     }
 }
--- src/com/shubham/addressbook/Contact.java
+++ src/com/shubham/addressbook/Contact.java
@@ -11,4 +11,56 @@ public class Contact {
     private String email;
 
 
+
+    public void setFirstName(String firstName){
+        this.firstName=firstName;
+    }
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public void setAddress(String address){
+        this.address=address;
+    }
+    public void setCity(String city){
+        this.city=city;
+    }
+    public void setState(String state){
+        this.state=state;
+    }
+    public void setZipcode(String zipcode){
+        this.zipcode=zipcode;
+    }
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber=phoneNumber;
+    }
+
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public String getFirstName()
+    {
+        return this.firstName;
+    }
+    public String getLastName(){
+        return this.lastName;
+    }
+    public String getAddress(){
+        return this.address;
+    }
+    public String getCity(){
+        return this.city;
+    }
+    public String getState(){
+        return this.state;
+    }
+    public String getZipcode(){
+        return this.zipcode;
+    }
+    public String getPhoneNumber(){
+        return this.phoneNumber;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+
 }
